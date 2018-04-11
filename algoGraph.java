d33f858a9dc8cd52fd22500bfef40bb1827914f6
francikz <francikz@miamioh.edu>
2018-04-11 10:12:33 -0400

    idk

--- algoGraph.java
+++ algoGraph.java
@@ -1,24 +1,37 @@
 public class algoGraph{
-    
-    public class Node{
-        private String label;
-        private List<Edge> edges;
-    
-        public void addEdge(Edge e){
-            edges.add(e);
-        }
-        public void setLabel(String s){
-            label = s;
-        }
+    private int nodeCount = 0;
+
+    // Use a doubly-linked list (or singly linked list and just save the previous node in a variable, idc.) 
+    // to store the nodes, each node has the list of edges
+    public algoGraph(){
+
     }
-    
-    public class Edge{
-        private Node destination;
-        private double weight;
-    
-    }
-
-
-
 }
 
+public class Node{
+    private String label;
+    private List<Edge> edges;
+
+    public Node(String label){
+        setLabel(label);
+    }
+
+    public void addEdge(Edge e){
+        edges.add(e);
+    }
+    public void setLabel(String s){
+        label = s;
+    }
+}
+
+public class Edge{
+    private Node source;
+    private Node dest;
+    private double weight;
+
+    public Edge(Node source, Node dest, double weight){
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
