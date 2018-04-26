3057d0e6329b929545f6ad996ba420c286d75799
francikz <francikz@miamioh.edu>
2018-04-26 16:50:43 -0400

    Added like 3 lines but I'm done with work so I have to save this somewhere

--- PathFinder.java
+++ PathFinder.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PathFinder{
     // soon to be a beautiful path finding program that everyone will adore
 
@@ -9,8 +11,14 @@ public class PathFinder{
 
     // modified binary sort thing
     // takes in a set of the edges that it will split to decide which edge to take.
-    private Vertex routeToTake(Set<Edge> edges){
-
+    private Vertex routeToTake(Set<Edge> e){
+        Set<Edge> edges = e;
+        Random rand = new Random();
+        for(int i = 0; i < edges.size(); i++){
+            // generate the random each time. Will probably change this up but is here to make sure we know to do it 
+            // on every loop
+            int n = rand.nextInt(51);
+        }
         // placeholder return for error purposes
         return null;
     }
