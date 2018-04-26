3183364a646709bc7e611ed3306f4583aa4fdf0e
francikz <francikz@miamioh.edu>
2018-04-26 16:38:47 -0400

    Started implementing the actual algorithm

--- PathFinder.java
+++ PathFinder.java
@@ -1,8 +1,17 @@
 public class PathFinder{
     // soon to be a beautiful path finding program that everyone will adore
 
-    public PathFinder(algoGraph ag){
-        
+    // returns a list of vertices which is the path to follow
+    // i.e. (List = {A, B, C, D} meaning the path is A -> B -> C -> D)
+    public List<Vertex> findPath(algoGraph g, Vertex start, double dist){
+
     }
 
+    // modified binary sort thing
+    // takes in a set of the edges that it will split to decide which edge to take.
+    private Vertex routeToTake(Set<Edge> edges){
+
+        // placeholder return for error purposes
+        return null;
+    }
 }
--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main{
     // main class to build the gui and such in
+    public static void main(String[] args){
 
-
-
+    }
 }
